041fe4dd0db9ca4075a29713d1295dba2ba4aefb
Mimikkip <33765208+Mimikkip@users.noreply.github.com>
2017-11-26 16:58:49 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,7 +40,7 @@ client.on('message', message => {
 });
 client.on('message', message => {
 	if (message.author === client.user) return;
-	if (message.content.startsWith(prefix + 'Help')) {
+	if (message.content.startsWith(prefix + 'help')) {
 	
 		 message.channel.send(`m.help m.ping m.hello m.xd m.mimikkip`);
 	}

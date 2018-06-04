f49628a4a782b99f08b0060a4a82ac9e371e3be3
Mimikkip <33765208+Mimikkip@users.noreply.github.com>
2018-06-04 23:44:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,25 +6,22 @@ client.on('ready',() => {
 	   client.user.setPresence({game: {name: "Fire Emblem Heroes", type: 0}});
 });
 
-var prefix = "m."
+var prefix = "l."
 
 client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith(prefix + 'help')) {
 	
 		 message.channel.send(`
-**m.help**
-**m.ping** 
-**m.hello**
-**m.xd**
-**m.mimikkip**
-**m.channel**
-**m.tetsuo**
+**l.help**
+**l.prefix**
+**l.summon**
+**l.castle**
 
 `);
 	}
 });
-
+//Copy and paste from here to add a command, replace mimikkip with ur command and here goes ur reply to wut bot is going to say
 client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith(prefix + 'ping')) {
@@ -32,43 +29,12 @@ client.on('message', message => {
 		 message.channel.send(`Pong! :ping_pong:  \`${Date.now() - message.createdTimestamp} ms\``);
 	}
 });
-client.on('message', message => {
-	if (message.author === client.user) return;
-	if (message.content.startsWith(prefix + 'hello')) {
-	
-		 message.channel.send(`Hello ${message.author.username}! :wave:`);
-		
-	}
-});
-client.on('message', message => {
-	if (message.author === client.user) return;
-	if (message.content.startsWith(prefix + 'xd')) {
-	
-		 message.channel.send(`XD`);
-	}
-});
-//Copy and paste from here to add a command, replace mimikkip with ur command and here goes ur reply to wut bot is going to say
-client.on('message', message => {
-	if (message.author === client.user) return;
-	if (message.content.startsWith(prefix + 'mimikkip')) {
-	
-		 message.channel.send(`Now trying to contact <@285845218639740929> ... Please wait`);
-	}
-});
 
 client.on('message', message => {
 	if (message.author === client.user) return;
-	if (message.content.startsWith(prefix + 'channel')) {
+	if (message.content.startsWith(prefix + 'prefix')) {
 	
-		 message.channel.send(`https://www.youtube.com/channel/UCCxNC6gwdCUivWQhkmSSh1A?view_as=subscriber`);
-	}
-});
-
-client.on('message', message => {
-	if (message.author === client.user) return;
-	if (message.content.startsWith(prefix + 'tetsuo')) {
-	
-		 message.channel.send(`Ohayo, Tetsuo-kun!`);
+		 message.channel.send(`The current prefix is l. (last updated 04/06/2018)`);
 	}
 });
 

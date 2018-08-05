cc80b5deda0eb3d631f48df779785a97d61e0d4c
Mimikkip <33765208+Mimikkip@users.noreply.github.com>
2018-08-05 13:29:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,12 +33,7 @@ client.on('message', message => {
         const embed = new Discord.RichEmbed()
   .setTitle("Update Logs")
   .setColor(0x7401DF)
-  .setDescription(
-<p
-"1.0 - Creation"
-"1.01 - added test command"
-</p>
-)
+  .setDescription( "1.0 - Creation" "1.01 - added test command")
 
 
   message.channel.send({embed});
